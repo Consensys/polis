4176a794f1153275fbbb5cee7fdd105d8fd567aa
meowyx <qafka256@gmail.com>
2023-03-28 22:46:41 +0600

    initialized polis repo

---
+++ components/Text.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Text = () => {
+  return (
+    <div>
+      <p className="py-6 text-5xl font-mono">
+        <b className="underline decoration-wavy decoration-yellow-400">Polis</b>
+        🍃
+      </p>
+    </div>
+  );
+};
+
+export default Text;
