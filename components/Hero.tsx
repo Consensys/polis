7061f260136307caf576f2c660250b772468cf50
meowyx <qafka256@gmail.com>
2023-04-01 02:00:32 +0600

    hero and searchbar done 42 percent

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,20 +19,31 @@ const Hero = () => {
           </p>
 
           <div className="flex flex-col mt-8 space-y-3 sm:-mx-2 sm:flex-row sm:justify-center sm:space-y-0">
-            <form>
+            <div className="flex items-center w-[476px] h-[52px] rounded-xl focus-within:shadow-lg bg-white">
               <input
+                className="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
                 type="search"
-                className="px-6 py-3 w-[476px] h-[52px] text-gray-700 bg-white border rounded-xl dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring sm:mx-2"
+                id="search"
                 placeholder="Search for apps, tools, libraries..."
               />
 
-              <button
-                type="button"
-                className="px-4 py-2 font-medium text-gray-600 transition-colors duration-200 sm:px-6 dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
-              >
-                Search
-              </button>
-            </form>
+              <div className="grid place-items-center h-full w-12 text-gray-300">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </div>
+            </div>
           </div>
         </div>
       </section>
