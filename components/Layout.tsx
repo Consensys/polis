a47266a1d2684154a9ba71648c61c2172891df6f
Alejandro <smarketing.cuz@gmail.com>
2023-05-30 12:44:37 -0400

    Layout, nav bar and text components created

---
+++ components/Layout.tsx
@@ -0,0 +1,43 @@
+import Head from "next/head";
+import { twMerge } from "tailwind-merge";
+import { Nav } from "./Nav";
+
+type Props = {
+  description?: string;
+  title?: string;
+  className?: string;
+  children: React.ReactNode;
+};
+
+const Layout: React.FC<Props> = ({
+  children,
+  title,
+  description,
+  className,
+}) => {
+  const rootClassName = twMerge(
+    "relative px-4 md:px-24 text-primary-black min-h-screen",
+    className ? className : ""
+  );
+  return (
+    <div className={rootClassName}>
+      <div className="max-w-screen-xl mx-auto">
+        <Head>
+          <title>{title}</title>
+          <meta name="description" content={`${description}`} />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Nav />
+        <main className="">{children}</main>
+      </div>
+      <div className="-z-10 w-full h-[130vh] absolute top-5 left-0 bg-hero-pattern bg-no-repeat bg-cover lg:bg-[length:100%_100%]"></div>
+    </div>
+  );
+};
+
+Layout.defaultProps = {
+  title: "Polis",
+  description: "A decentralized DAO marketplace for ConsenSys example Dapps",
+};
+
+export default Layout;
--- components/Text.tsx
+++ components/Text.tsx
@@ -1,15 +1,63 @@
-import Image from "next/image";
 import Link from "next/link";
+import { HTMLProps, ReactElement } from "react";
+import React from "react";
+import { UrlProps } from "./types";
+import { twMerge } from "tailwind-merge";
 
-const Text = () => {
-  return (
-    <div>
-      <p className="py-6 text-5xl font-mono">
-        <b className="underline decoration-wavy decoration-yellow-400">Polis</b>
-        🍃
-      </p>
-    </div>
+export const H1 = (props: HTMLProps<HTMLHeadingElement>): ReactElement => {
+  const rootClassName = twMerge(
+    "text-primary block text-3xl leading-8 font-bold tracking-tight md:text-6xl",
+    props.className ? props.className : ""
   );
+
+  return <h1 {...props} className={rootClassName} />;
 };
 
-export default Text;
+export const H2 = (props: HTMLProps<HTMLHeadingElement>): ReactElement => {
+  const rootClassName = twMerge(
+    "block text-xl leading-8 font-extrabold tracking-tight md:text-3xl",
+    props.className ? props.className : ""
+  );
+
+  return <h2 {...props} className={rootClassName} />;
+};
+
+export const H3 = (props: HTMLProps<HTMLHeadingElement>): ReactElement => {
+  const rootClassName = twMerge(
+    "block text-lg leading-8 font-bold tracking-tight md:text-xl",
+    props.className ? props.className : ""
+  );
+
+  return <h3 {...props} className={rootClassName} />;
+};
+
+export const H4 = (props: HTMLProps<HTMLHeadingElement>): ReactElement => {
+  const rootClassName = twMerge(
+    "block text-md leading-8 font-extrabold tracking-tight md:text-lg",
+    props.className ? props.className : ""
+  );
+
+  return <h4 {...props} className={rootClassName} />;
+};
+
+export const Text = (props: HTMLProps<HTMLParagraphElement>): ReactElement => {
+  const rootClassName = twMerge(
+    "text-primary text-opacity-70",
+    props.className ? props.className : ""
+  );
+
+  return <p {...props} className={rootClassName} />;
+};
+
+export const Url = ({ text, ...props }: UrlProps): ReactElement => {
+  const rootClassName = twMerge(
+    "cursor-pointer underline text-base leading-normal",
+    props.className ? props.className : ""
+  );
+
+  return (
+    <Link passHref target="_blank" {...props} className={rootClassName}>
+      {text}
+    </Link>
+  );
+};
