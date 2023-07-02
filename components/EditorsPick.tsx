0e093fd40f7073c8cb783e6039beb4846b743d42
meowyx <qafka256@gmail.com>
2023-07-02 23:15:42 +0600

    added dark attribute on home page editorspick & all apps

--- components/EditorsPick.tsx
+++ components/EditorsPick.tsx
@@ -4,12 +4,11 @@ import { H2 } from "./Text";
 import { getApplications } from "../lib/applications";
 
 type EditorsPickProps = {
-  applications: IApplication[]
-}
+  applications: IApplication[];
+};
 
-const EditorsPick: FC<EditorsPickProps>= ( { applications }) => {
+const EditorsPick: FC<EditorsPickProps> = ({ applications }) => {
   // Filter the app card data to include only the editor's pick cards
-  
 
   // const editorPicked = allApplications.filter(
   //   (application) => application.isEditorsPick
@@ -17,11 +16,11 @@ const EditorsPick: FC<EditorsPickProps>= ( { applications }) => {
 
   return (
     <div className="container px-6 pb-2 mx-auto mt-24">
-      <H2 className="text-center text-transparent mb-14 bg-gradient-to-br bg-clip-text from-primary to-slate-500">
+      <H2 className="text-center text-transparent mb-14 bg-gradient-to-br bg-clip-text from-primary to-slate-500 dark:text-gray-300">
         Our Editor&apos;s Pick
       </H2>
       <div className="grid justify-center gap-4 cursor-pointer md:grid-cols-2 lg:grid-cols-3 md:gap-6 lg:gap-12">
-        {applications?.map(data => (
+        {applications?.map((data) => (
           <AppCard key={data.id} application={data} />
         ))}
       </div>
