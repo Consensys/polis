fcf7a1519c99fe02dfb4fcf7c7ae8c42c6e7d807
Chinthaka <thinkaloud.lk@gmail.com>
2023-07-19 16:39:00 +0530

    cli tool to create an empty db

--- lib/ipns.ts
+++ lib/ipns.ts
@@ -1,19 +1,5 @@
 import * as Name from "w3name";
 
-export const publish = async (
-  content: string
-): Promise<{ name: string; key: Uint8Array }> => {
-  const name = await Name.create();
-
-  const revision = await Name.v0(name, content);
-  await Name.publish(revision, name.key);
-
-  return {
-    name: name.toString(),
-    key: name.key.bytes,
-  };
-};
-
 export const update = async (ipns: string, keyStr: string, content: string) => {
   const name = Name.parse(ipns);
   const revision = await Name.resolve(name);
@@ -23,7 +9,7 @@ export const update = async (ipns: string, keyStr: string, content: string) => {
   if (!keyStr) {
     return;
   }
-  const key = new Uint8Array(JSON.parse(keyStr));
+  const key = new Uint8Array(Buffer.from(keyStr, "base64"));
 
   const name2 = await Name.from(key);
   await Name.publish(nextRevision, name2.key);
@@ -39,16 +25,4 @@ export const getcurrentHash = async () => {
   const res = await fetch(`${nameServiceUrl}/${process.env.DB_HASH}`);
 
   return (await res.json()).value;
-};
-
-export const resolve = async () => {
-  const keyStr = localStorage.getItem("name-key");
-  if (!keyStr) {
-    return;
-  }
-  const key = new Uint8Array(JSON.parse(keyStr));
-
-  const name = await Name.from(key);
-  const revition = await Name.resolve(name);
-  return revition.value;
 };
