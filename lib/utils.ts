d02b770a387107f49b5e3792a7367d3ecb22ee2c
Alejandro <smarketing.cuz@gmail.com>
2023-12-21 14:29:13 -0500

    Add isAllowedEditor function and use it in AppHeader component

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,10 @@
 import { ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+const ALLOW_LIST = process.env.ALLOW_LIST?.split(",") || [];
+
 export const  cn= (...inputs: ClassValue[]) => {
     return twMerge(clsx(inputs))
   }
+
+ export const isAllowedEditor = (address: string) => ALLOW_LIST.includes(address);
